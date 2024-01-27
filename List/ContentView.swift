a1bcb17950a4d75557156ef71d5f9a0def4876bb
ged <ged599@outlook.com>
2024-01-27 14:29:58 -0500

    Initial Commit

---
+++ List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  List
+//
+//  Created by Glenn Dufour on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Glenn Dufour on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
